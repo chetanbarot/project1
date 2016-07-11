a2e29102732559e9c2dcd4d1512460fa5c0999b7
chetan barot <chetanbarot@hotmail.com>
2016-07-11 19:27:15 +0100

    most of the function working

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+var gameboard =document.getElementById('gameboard');
+var base=document.getElementById('bottom-bar');
+
+// var alertbox =
+// "<p id='alertbox'>You win! Your discount is congrats!</p>" 
+// divElement.innerHTML = alertbox;
+
+// // Appending the div element to body
+// document.getElementsByTagName("body")[0].appendChild(divElement);
+
+var alert=document.getElementById('alertbox');
+
+window.onload = function(){
+  gameboard.style.display="none";
+  base.style.display="none";
+  alert.style.display="none";
+}
 // here I have created a variable buttons for my boxes
 var buttons = document.getElementsByClassName('back');
 // var items = [' red',' pink',' blue',' green'];
@@ -69,10 +86,10 @@ function checkForMatch(){
   click1 = 0;
   click2 = 0;
 }
-listenForClick();
 
 
 var seconds = 120;
+
 function secondPassed() {
     var minutes = Math.round((seconds - 30)/60);
     var remainingSeconds = seconds % 60;
@@ -80,17 +97,19 @@ function secondPassed() {
         remainingSeconds = "0" + remainingSeconds;  
     }
     document.getElementById('countdown').innerHTML = minutes + ":" + remainingSeconds;
-    if (seconds == 0) {
-        clearInterval(countdownTimer);
+    if (seconds == 0) { 
+        //clearInterval(countdownTimer);
         document.getElementById('countdown').innerHTML = "Spiled";
-        // document.getElementsByClassName('front').innerHTML =="back";
+        document.getElementById('percentage').innerHTML ="";
+        alert.style.display = "block"
+        document.getElementById('alertbox').innerHTML = "Congrats you win " + percentage.innerHTML + "great!"
+        gameboard.style.display = "none"
+        base.style.display="none"
         resetTheGame();
     } else {
-
         seconds--;
     }
 }
-var countdownTimer = setInterval('secondPassed()', 1000);
 
 // function move() {
 //   var elem = document.getElementById("myBar");
@@ -112,29 +131,33 @@ function increaseTheBar(){
 
   if (score === 1) {
     myBar.setAttribute("style", "width: 8.33%");
-    // document.getElement (grab a div from the html and add something to it's innerHTML)
+    // document.getElementById('percentage').innerHTML ="5%(discount)" (grab a div from the html and add something to it's innerHTML)
   } else if(score === 2) {
     myBar.setAttribute("style", "width: 16.66%");
   } else if(score === 3) {
     myBar.setAttribute("style", "width: 24.99%");
+    document.getElementById('myBar').innerHTML ="You Earned 5% Discount"
   } else if(score === 4) {
     myBar.setAttribute("style", "width: 33.32%");
   } else if(score === 5) {
     myBar.setAttribute("style", "width: 41.65%");
   } else if(score === 6) {
     myBar.setAttribute("style", "width: 49.98%");
+    document.getElementById('myBar').innerHTML ="You Earned 10% Discount"
   } else if(score === 7) {
     myBar.setAttribute("style", "width: 58.31%");
   } else if(score === 8) {
     myBar.setAttribute("style", "width: 66.64%");
   } else if(score === 9) {
     myBar.setAttribute("style", "width: 74.97%");
+    document.getElementById('myBar').innerHTML ="You Earned 15% Discount"
   } else if(score === 10) {
     myBar.setAttribute("style", "width: 83.30%");
   } else if(score === 11) {
     myBar.setAttribute("style", "width: 93.63%");
   } else if(score === 12) {
     myBar.setAttribute("style", "width: 100%");
+    document.getElementById('myBar').innerHTML ="Hurray!! You Earned 20% Discount"
   }
 
   // var back = (24 - backleft) * 20; // 20 points for each successful back=480 pts
@@ -162,7 +185,12 @@ function resetTheGame(){
   console.log("resetting the game");
   for(var i = 0; i < squares.length; i++) {
     squares[i].className = "square back";
+    // need to reset the time over here
   }
+  var countdown = document.getElementById("countdown")
+  // clearInterval(countdownTimer)
+  countdown.style.display="none";
+  console.log(countdownTimer)
 
   myBar.setAttribute("style", "width: 0%");
   click1 = 0;
@@ -171,11 +199,47 @@ function resetTheGame(){
 }
 
 
+// Creating a div element
+var divElement = document.createElement("Div");
+divElement.id = "welcome-box";
+
+// Styling it
+divElement.style.textAlign = "center";
+divElement.style.fontWeight = "bold";
+// divElement.style.fontSize = "smaller";
+divElement.style.paddingTop = "15px";
+
+var welcomeBox =
+"<p>Welcome to the Black Sheep challenge...</p>" +
+"<p>Black Sheep was started by a group of coffee-loving friends from uni who decided to Leave The Herd Behind and stay awake for the rest of their lives. They set out to rid the world of boring, average-tasting coffee and committed to rethink long-established traditions and always challenge the status quo by only sourcing unorthodox coffees that have a story to be told. <br> We have decided to give you a 30% discount on your coffee if you challange your memory with the game as we think that would lead to a lot less wars and voilence right ....</p>" + 
+// "<p>Welcome to the Black Sheep challenge...</p>"
+"<input></input><br>" +
+"<button id='playGame'>Submit email address & start game</button>"
+divElement.innerHTML = welcomeBox;
+
+// Appending the div element to body
+document.getElementsByTagName("body")[0].appendChild(divElement);
 
 
+var playGame = document.getElementById("playGame");
 
+playGame.addEventListener("click", function(){
+  gameboard.style.display = "block"
+  base.style.display="block"
+  this.parentNode.style.display = "none";
+ 
+  listenForClick();
+  var countdownTimer = setInterval('secondPassed()', 1000);
 
+})
 
+var end = document.getElementById("end");
+
+end.addEventListener("click", function(){
+  gameboard.style.display = "none"
+  base.style.display="none"
+  alert.style.display = "block"
+})
 
 
 
